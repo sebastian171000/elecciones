093798c925ddb1e14dce7db0c0a9403701cd2ce1
sebastian171000 <smpl3000@gmail.com>
2020-12-13 18:57:22 -0500

    app.js fixed

--- js/app.js
+++ js/app.js
@@ -32,8 +32,13 @@ if(inputDni){
 let buttonCard = document.querySelectorAll('.card button');
 let greenButton = document.querySelector('.boton-votar');
 let votoFinalizado = document.querySelector('.vota-finalizado');
-greenButton.style.display = 'none';
-votoFinalizado.style.display = 'none';
+if(greenButton){
+    greenButton.style.display = 'none';
+}
+if(votoFinalizado){
+    votoFinalizado.style.display = 'none';
+}
+
 for (let index = 0; index < buttonCard.length; index++) {
     const button = buttonCard[index];
     button.addEventListener('click', () => {
@@ -49,33 +54,35 @@ for (let index = 0; index < buttonCard.length; index++) {
     });
     
 }
-
-greenButton.addEventListener('click', () => {
-    Swal.fire({
-        title: '¡Voto realizado!',
-        text: 'Muchas gracias por participar',
-        imageUrl: 'img/ico_success@2x.png',
-        imageWidth: 55,
-        imageAlt: 'Custom image',
-        showCloseButton: true,
-        showConfirmButton: false,
-        customClass: 'swal-voto-realizado',
-        backdrop: `rgba(0,0,0,0.8)`
-    }).then(() => {
-        greenButton.style.display = 'none';
-        votoFinalizado.style.display = 'block';
-        for (let index = 0; index < buttonCard.length; index++) {
-            buttonCard[index].style.display = 'none';
+if(greenButton){
+    greenButton.addEventListener('click', () => {
+        Swal.fire({
+            title: '¡Voto realizado!',
+            text: 'Muchas gracias por participar',
+            imageUrl: 'img/ico_success@2x.png',
+            imageWidth: 55,
+            imageAlt: 'Custom image',
+            showCloseButton: true,
+            showConfirmButton: false,
+            customClass: 'swal-voto-realizado',
+            backdrop: `rgba(0,0,0,0.8)`
+        }).then(() => {
+            greenButton.style.display = 'none';
+            votoFinalizado.style.display = 'block';
+            for (let index = 0; index < buttonCard.length; index++) {
+                buttonCard[index].style.display = 'none';
+                
+            }
             
-        }
-        
-        let d = new Date();
-        let h = d.getHours();
-        let m = d.getMinutes();
-        votoFinalizado.innerHTML = `<p>Usted realizó su voto hoy a las ${h}:${m}</p>`;
-        
-    })
-});
+            let d = new Date();
+            let h = d.getHours();
+            let m = d.getMinutes();
+            votoFinalizado.innerHTML = `<p>Usted realizó su voto hoy a las ${h}:${m}</p>`;
+            
+        })
+    });
+}
+
 
 
 
